89ccd81830da04fcfea568d707b9b5a4448f2979
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-05-08 22:24:00 +0200

    Update 0-generator.py

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -5,12 +5,17 @@ from time import sleep
 import datetime
 
 for i in range(10000):
-    sleep(random.uniform(0, 0.01))  # faster output
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+    sleep(random.uniform(0, 0.01))
+    ip = "{}.{}.{}.{}".format(
         random.randint(1, 255), random.randint(1, 255),
-        random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+        random.randint(1, 255), random.randint(1, 255)
+    )
+    timestamp = datetime.datetime.now()
+    status = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
+    size = random.randint(1, 1024)
+    log_line = (
+        f"{ip} - [{timestamp}] "
+        f"\"GET /projects/260 HTTP/1.1\" {status} {size}\n"
+    )
+    sys.stdout.write(log_line)
     sys.stdout.flush()
